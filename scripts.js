94bdc60b01b62442e2a3bd41e672c7c8191a2c0a
Maria Jessen <mjessen@dal.ca>
2020-06-08 19:36:54 -0300

    use axios

--- scripts.js
+++ scripts.js
@@ -1,13 +1,26 @@
-var request = new XMLHttpRequest();
+import axios from 'axios';
+//const axios = require("axios");
+const api = axios.create({ baseURL: "https://54.144.197.204:5000/"});
 
+async function getPassword() {
+  try {
+    const data = await api.get("passwordplease");
+    console.log("HERE IS THE DATA: " + data);
+    var rootDiv = document.getElementById('root');
+    rootDiv.innerHTML = data;
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+/*
 //localhost
 request.open('GET', 'https://54.144.197.204:5000/passwordplease');
 
 request.onload = function() {
   var data = this.response;
-  console.log("HERE IS THE DATA: " + data);
-  var rootDiv = document.getElementById('root');
-  rootDiv.innerHTML = data;
+
 }
 
 request.send()
+*/
